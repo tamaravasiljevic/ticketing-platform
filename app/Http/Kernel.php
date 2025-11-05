49cde05bb051c8a1c865f7e191199454b8863833
tamaravasiljevic <tamaravasiljevic89@gmail.com>
2025-11-05 13:41:37 +0100

    - small tweaks

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -48,7 +48,6 @@ class Kernel extends HttpKernel
         'auth.session' => \Illuminate\Session\Middleware\AuthenticateSession::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
-//        'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class
